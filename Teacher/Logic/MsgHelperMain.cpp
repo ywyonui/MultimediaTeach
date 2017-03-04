3c7f2cddc114b56273a46b5d1d06946b700ca64f
ywyonui <ywyonui@cdeway.com>
2017-03-05 00:28:40 +0800

    添加消息处理器

---
+++ Teacher/Logic/MsgHelperMain.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "MsgHelperMain.h"
+
+#include <iostream>
+using namespace std;
+
+
+
+CMsgHelperMain::CMsgHelperMain()
+{
+}
+
+
+CMsgHelperMain::~CMsgHelperMain()
+{
+}
+
+
+#pragma region 公共接口
+/*************************************************************
+函数名称:	GetInstance
+参数说明:	void
+返 回 值:	CMsgHelperMain：服务器消息处理对象
+功能说明:	返回单例对象
+*************************************************************/
+CMsgHelperMain& CMsgHelperMain::GetInstance(void)
+{
+	static CMsgHelperMain m_hInstance;
+	return m_hInstance;
+}
+
+
+/*************************************************************
+函数名称:	NetMsgCallBack
+参数说明:	dwID:	客户端Socket
+			vParam:	数据
+			nLen:	数据长度
+返 回 值:	void
+功能说明:	处理消息的回调函数，基类中可以做一些通用的处理，具体应该由派生类实现
+*************************************************************/
+void CMsgHelperMain::NetMsgCallBack(DWORD dwID, void* vParam, int nLen)
+{
+	
+}
+
+/*************************************************************
+函数名称:	SetHwnd
+参数说明:	hWnd:	关联窗口
+返 回 值:	void
+功能说明:	设置一个窗口，用于发送消息，处理UI相关信息
+*************************************************************/
+void CMsgHelperMain::SetHwnd(HWND hWnd)
+{
+	m_hWnd = hWnd;
+}
+
+/*************************************************************
+函数名称:	GetHwnd
+参数说明:	void
+返 回 值:	hWnd:	关联窗口
+功能说明:	返回关联窗口
+*************************************************************/
+HWND CMsgHelperMain::GetHwnd(void)
+{
+	return m_hWnd;
+}
+#pragma endregion 公共接口
+
+
+#pragma region 私有接口
+
+
+#pragma endregion 私有接口
